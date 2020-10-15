5fdafbb4eb0d74e8da0e92f1d5f5472605c490ed
Sam8892 <saddem.laifi1920@outlook.com>
2020-10-15 18:24:13 +0200

    clean project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
+
 import 'package:flutter_auth/constants.dart';
 import 'package:flutter_auth/Home.dart';
 import 'package:flutter_auth/main.dart';
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: WelcomeScreen(),
+      home: Home(),
     );
   }
 }
